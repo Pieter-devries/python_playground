46dc8eb36ca677039c34f1b8de9405144351c098
Pieter-devries <35712925+Pieter-devries@users.noreply.github.com>
2022-05-25 12:28:03 +0900

    Update deckofcards.py

--- deckofcards.py
+++ deckofcards.py
@@ -1,23 +1,48 @@
 #!/usr/bin/env python3
 import random
 from pprint import pprint as pprint
+import re
+
+# Current Status:
+# Deck is created and player can draw and discard cards to create/improve their hand
+# Sets are displayed, but not evaluated
+# Sequences are not displayed and not evaluated
+# Computer player is not programmed
+# End goal: Can player single poker and win if you have a better hand then the CP
+
 def main():
+    # Setup variables
     global deck
     global hand
+    global suits
+    suits = {"Spades", "Hearts", "Clubs", "Diamonds"}
     deck = {}
-    deck = make_deck()
-    hand = draw_hand()
-    print(f'Your hand contains {hand}')
+    hand = []
+    turn = 1
+    continue_game = True
+    while continue_game:
+        deck = make_deck()
+        hand = draw_hand(turn)
+        sets = create_sets(hand)
+        display_hand(hand, sets)
+        response = input()
+        if int(response) == 0:  # Quit Game
+            print('Thanks for playing!')
+            continue_game = False
+        discard_choice = hand[int(response) - 1]
+        discard_card(discard_choice)
+        turn += 1
 
+# Create initial deck
 def make_deck():
     global deck
-    suits = {"Spades", "Hearts", "Clubs", "Diamonds"}
-
+    global suits
     for key in suits:
         if key not in deck:
             deck[key] = list()
             deck[key].extend(make_suit(key))
     return deck
+# Helper function for making the deck
 def make_suit(key):
     numbered_cards = range(1, 11)
     special = ["Ace", "Jack", "Queen", "King"]
@@ -33,9 +58,10 @@ def make_suit(key):
     return list_of_values
 
     dict[key].extend()
+# Function to remove cards from the deck and add into the player's hand
 def draw_hand(turn = 1):
     global deck
-    hand = []
+    global hand
     if turn == 1:
         for x in range(7):
             suit = random.choice(list(deck.values()))
@@ -43,20 +69,58 @@ def draw_hand(turn = 1):
             hand.append(card)
             suit = search(deck, card)
             remove_card(suit, card)
-            # pprint(suit)
-            # pprint(card)
-            # pprint(deck)
+    else:
+        suit = random.choice(list(deck.values()))
+        card = random.choice(list(suit))
+        hand.append(card)
+        suit = search(deck, card)
+        remove_card(suit, card)
     return hand
     return deck
+# Helper function for drawing hand
 def search(dict, lookup):
     for key, value in dict.items():
         for v in value:
             if lookup in v:
                 return key
+# Helper function for drawing hand
 def remove_card(suit, card):
     global deck
     deck[suit] = [s for s in deck[suit] if card not in s]
     return deck
-
+# Function to show hand to player
+def display_hand(hand, sets):
+    print(f'Your hand contains')
+    pprint(hand)
+    print(sets)
+    print('Choose a card to discard by typing 1,2,3 etc. or quit by typing 0.')
+# Function to remove cards from player's hand
+def discard_card(card):
+    global hand
+    hand = [s for s in hand if card not in s]
+    return hand
+# Function for creating sets
+def create_sets(hand):
+    values = first_value(hand)
+    duplicates = []
+    for value in values:
+        dupes = values.count(value)
+        if dupes > 1:
+            duplicates.append(f'{dupes} of {value}s')
+    if duplicates:
+        sets = remove_duplicates(duplicates)
+        return sets
+    sets = "No sets"
+    return sets
+# Helper function for creating sets and sequences
+def first_value(hand):
+    values = []
+    for card in hand:
+        value = re.search('([\S]+)', card).group()
+        values.append(value)
+    return values
+# Helper function for creating sets
+def remove_duplicates(sets):
+    return list(dict.fromkeys(sets))
 
 if __name__ == '__main__': main()
